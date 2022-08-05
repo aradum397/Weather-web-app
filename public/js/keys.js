76916fcb03164b60617b4e4a4967cf070507ef8f
aradum397 <soyshpardt@gmail.com>
2022-08-05 20:09:37 +0300

    xxx

---
+++ public/js/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+	mapBoxToken: 'pk.eyJ1Ijoic295YmVpbmciLCJhIjoiY2w1dDZsNzc4MDJtdTNkbGFoN3BjeWdpdyJ9.XmD6o607KhcV34SKFHkrAQ',
+	weatherStackToken: 'd0f52eb74a3b530fca293a67fb14b952'
+}
